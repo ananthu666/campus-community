5fca79a8a0ec3fc73d04a138e9c42d63c24d3c31
Adithyan T <adithsreedhar628@gmail.com>
2023-04-17 18:41:00 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Main from '../components/Main/Main';
 import Footer from '../components/Footer/Footer';
-import NavBar from '../components/NavBar/NavBar';
+import NavBar from '../components/NavBar/NavBar.js';
 
 function Home() {
   return (
